776b4b786fa1f85068faa2708f0d6fdf5646468a
codenameraziel <71160295+codenameraziel@users.noreply.github.com>
2026-02-28 10:15:33 -0300

    update

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+def main():
+    """Ponto de entrada para comandos administrativos do Django."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'obico.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Nao foi possivel importar Django. Esta instalado?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
